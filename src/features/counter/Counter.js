961a7b9889bd2dc12514e505f14ed674ebceda40
Richard Perdomo <richard.perdomo121@gmail.com>
2021-07-22 19:37:52 +0200

    creating todos with redux

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,67 +1,62 @@
-import React, { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import React, { useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 import {
-  decrement,
+  add,
   increment,
   incrementByAmount,
   incrementAsync,
   incrementIfOdd,
-  selectCount,
-} from './counterSlice';
-import styles from './Counter.module.css';
+  selectTodos,
+} from './counterSlice'
+import styles from './Counter.module.css'
 
 export function Counter() {
-  const count = useSelector(selectCount);
-  const dispatch = useDispatch();
-  const [incrementAmount, setIncrementAmount] = useState('2');
+  const todos = useSelector(selectTodos)
+  const dispatch = useDispatch()
 
-  const incrementValue = Number(incrementAmount) || 0;
+  const [todo, setTodo] = useState({ name: '', important: false, id: 1 })
+
+  const handleChange = (e) => {
+    setTodo((prevState) => {
+      return { ...prevState, name: e.target.value }
+    })
+  }
+
+  const handleImportance = (e) => {
+    setTodo((prevState) => {
+      return {
+        ...prevState,
+        important: e.target.value === 'important' ? true : false,
+      }
+    })
+  }
+
+  const handleSend = () => {
+    dispatch(add(todo))
+
+    setTodo((prevState) => {
+      return {
+        ...prevState,
+        important: false,
+        name: '',
+      }
+    })
+  }
 
   return (
     <div>
-      <div className={styles.row}>
-        <button
-          className={styles.button}
-          aria-label="Decrement value"
-          onClick={() => dispatch(decrement())}
-        >
-          -
-        </button>
-        <span className={styles.value}>{count}</span>
-        <button
-          className={styles.button}
-          aria-label="Increment value"
-          onClick={() => dispatch(increment())}
-        >
-          +
-        </button>
-      </div>
-      <div className={styles.row}>
-        <input
-          className={styles.textbox}
-          aria-label="Set increment amount"
-          value={incrementAmount}
-          onChange={(e) => setIncrementAmount(e.target.value)}
-        />
-        <button
-          className={styles.button}
-          onClick={() => dispatch(incrementByAmount(incrementValue))}
-        >
-          Add Amount
-        </button>
-        <button
-          className={styles.asyncButton}
-          onClick={() => dispatch(incrementAsync(incrementValue))}
-        >
-          Add Async
-        </button>
-        <button
-          className={styles.button}
-          onClick={() => dispatch(incrementIfOdd(incrementValue))}
-        >
-          Add If Odd
-        </button>
-      </div>
+      <input type="text" onChange={handleChange} />
+      <button onClick={handleSend}> add todo</button>
+      <select onChange={handleImportance}>
+        <option value="important">important</option>
+        <option value="notimportant">not important</option>
+      </select>
+
+      {todos.map((todo) => (
+        <div key={todo.id}>
+          {todo.name} {todo.important === true ? '❗️' : '😎'} {todo.id}
+        </div>
+      ))}
     </div>
-  );
+  )
 }
